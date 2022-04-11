75ad3a3c7a0762137fd2bed6b68d8a4ced1390f8
Abraham Serena <abraham.serena.lk@gmail.com>
2022-04-10 22:44:55 -0500

    Added Header component

---
+++ components/Header.jsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import React, { useContext } from 'react'
+
+const categories = [{ name: 'React', slug: 'react' }, { name: 'Web Development', slug: 'web-dev' }]
+
+const Header = () => {
+  return (
+    <div className='container mx-auto px-10 mb-8'>
+      <div className='border-b w-full inline-block border-blue-400 py-8'>
+        <div className='md:float-left block'>
+          <Link href='/'>
+            <span className='cursor-pointer font-bold text-4xl text-black'>
+              idkan.dev
+            </span>
+          </Link>
+        </div>
+        <div className='hidden md:float-left md:contents'>
+          {categories.map(category => (
+            <Link href={`/category/${category.slug}`} key={category.slug}>
+              <span className='md:float-right mt-2 align-middle text-black ml-4 font-semibold cursor-pointer'>
+                {category.name}
+              </span>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
