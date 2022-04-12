f90cb0cb0ca6a9ffcafeaaeee2226b6c114dbf78
Abraham Serena <abraham.serena.lk@gmail.com>
2022-04-11 23:01:41 -0500

    Added post design

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
+import moment from 'moment'
+import Link from 'next/link'
+import { BsCalendarDate } from 'react-icons/bs'
 
 const PostCard = ({ post }) => {
   return (
-    <div className='bg-white rounded shadow p-6'>
-      <h3 className='text-xl font-bold mb-4'>{post.title}</h3>
-      <p className='text-gray-700 text-base'>{post.excerpt}</p>
+    <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
+      <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
+        <img src={post.featuredImage?.url} alt={post.title} className='object-top absolute h-80 w-full object-cover rounded shadow-lg rounded-t-lg lg:rounded-lg' />
+      </div>
+      <h1 className='transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 texxt-3xl font-semibold'>
+        <Link href={`/post/${post.slug}`}>
+          {post.title}
+        </Link>
+      </h1>
+      <div className='block lg:flex text-center items-center justify-center mb-8 w-full'>
+        <div className='flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8'>
+          <img src={post.author.photo.url} alt={post.author.name} height='30px' width='30px' className='align-middle rounded-full' />
+          <p className='inline align-middle text-gray-700 ml-2 text-lg'>{post.author.name}</p>
+        </div>
+        <div className='font-medium text-gray-700'>
+          <BsCalendarDate className='inline mr-2' />
+          <span>
+            {moment(post.createdAt).format('MMMM DD YYYY')}
+          </span>
+        </div>
+      </div>
+      <p className='text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8'>
+        {post.excerpt}
+      </p>
+      <div className='text-center'>
+        <Link href={`/post/${post.slug}`}>
+          <span className='transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-8 cursor-pointer'>
+            Continue Reading
+          </span>
+        </Link>
+      </div>
     </div>
   )
 }
