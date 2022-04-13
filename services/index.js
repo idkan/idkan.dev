5fd638f0a09cf9f5b787b747303e93a5846419f0
Abraham Serena <abraham.serena.lk@gmail.com>
2022-04-12 23:26:44 -0500

    Added getRecetPosts grapcms query

--- services/index.js
+++ services/index.js
@@ -37,3 +37,23 @@ export const getPosts = async () => {
 
   return result.postsConnection.edges
 }
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query getPostDetails() {
+      posts(orderBy: createdAt_ASC
+        last:3 ) {
+          title
+          featuredImage {
+            url
+          }
+          createdAt
+          slug
+        }
+    }
+  `
+
+  const result = await request(graphqlAPI, query)
+
+  return result.posts
+}
