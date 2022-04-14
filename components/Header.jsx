8b00d157976a5c16c23776e728a0dc17d59d83ab
Abraham Serena <abraham.serena.lk@gmail.com>
2022-04-13 21:13:35 -0500

    Added Categories from grapCMS request into header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,17 @@
 import Link from 'next/link'
-import React, { useContext } from 'react'
-
-const categories = [{ name: 'React', slug: 'react' }, { name: 'Web Development', slug: 'web-dev' }]
+import React, { useContext, useState, useEffect } from 'react'
+import { getCategories } from '../services'
 
 const Header = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    getCategories().then((categories) => {
+      setCategories(categories)
+    })
+  }, [])
+
+  console.log(categories)
   return (
     <div className='container mx-auto px-10 mb-8'>
       <div className='border-b w-full inline-block border-blue-400 py-8'>
