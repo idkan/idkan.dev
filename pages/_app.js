51f32073d9b261264e3d1375adfc855958fcb401
Abraham Serena <abraham.serena.lk@gmail.com>
2022-04-13 21:57:16 -0500

    Remove unecesary dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.scss'
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Layout } from '../components'
 
 function MyApp ({ Component, pageProps }) {
