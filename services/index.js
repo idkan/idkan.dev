36f34865b52c8ee2a081635f20ce48a5e5cec1d9
Abraham Serena <abraham.serena.lk@gmail.com>
2022-04-13 21:56:45 -0500

    Addede getPostDetails service

--- services/index.js
+++ services/index.js
@@ -38,6 +38,40 @@ export const getPosts = async () => {
   return result.postsConnection.edges
 }
 
+export const getPostDetails = async (slug) => {
+  const query = gql`
+    query getPostDetails($slug: String!) {
+      post(where: { slug: $slug }) {
+        author {
+          bio
+          id
+          name
+          photo {
+            url
+          }
+        }
+        createdAt
+        slug
+        title
+        excerpt
+        featuredImage {
+          url
+        }
+        categories {
+          name
+          slug
+        }
+        content {
+          raw
+        }
+      }
+    }
+  `
+  const result = await request(graphqlAPI, query, { slug })
+
+  return result.post
+}
+
 export const getRecentPosts = async () => {
   const query = gql`
     query getPostDetails() {
@@ -57,7 +91,7 @@ export const getRecentPosts = async () => {
   return result.posts
 }
 
-export const getSimilarPosts = async (slug) => {
+export const getSimilarPosts = async (categories, slug) => {
   const query = gql`
     query getPostDetails($slug: String!, $categories: [String!]) {
       posts(where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}} last:3) {
@@ -70,7 +104,7 @@ export const getSimilarPosts = async (slug) => {
       }
     }
   `
-  const result = await request(graphqlAPI, query, { slug })
+  const result = await request(graphqlAPI, query, { categories, slug })
 
   return result.posts
 }
