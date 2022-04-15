1b05cf019717874bc453394b152c8f13db250279
Abraham Serena <abraham.serena.lk@gmail.com>
2022-04-15 02:12:01 -0500

    Fixed design on large devices

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -4,22 +4,24 @@ import { PostDetails, Categories, PostWidget, Author, Comments, CommentsForm } f
 
 const PostView = ({ post }) => {
   return (
-    <div className='conatiner mx-auto px-10 mb-8'>
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className='col-span-1 lg:col-sapn-8'>
-          <PostDetails post={post} />
-          <Author author={post.author} />
-          <CommentsForm slug={post.slug} />
-          <Comments slug={post.slug} />
-        </div>
-        <div className='col-span-1 lg:col-sapn-4'>
-          <div className='relative lg:sticky top-8'>
-            <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
-            <Categories />
+    <>
+      <div className='container mx-auto px-10 mb-8'>
+        <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+          <div className='col-span-1 lg:col-span-8'>
+            <PostDetails post={post} />
+            <Author author={post.author} />
+            <CommentsForm slug={post.slug} />
+            <Comments slug={post.slug} />
+          </div>
+          <div className='col-span-1 lg:col-span-4'>
+            <div className='relative lg:sticky top-8'>
+              <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
+              <Categories />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
