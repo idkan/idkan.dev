2600c2ed50ebd7371fb1e8057b41355471213917
Abraham Serena <abraham.serena.lk@gmail.com>
2022-04-19 12:20:33 -0500

    Added general site metadata

---
+++ data/siteMetadata.js
@@ -0,0 +1,29 @@
+export const siteMetadata = {
+  title: 'idkan dev 👨‍💻 | Backend, Frontend, JavaScript, Algorithms , Work Life',
+  author: 'Abraham Serena',
+  headerTitle: 'idkan.dev Blog',
+  description: 'Web Development, Algorithms, Work Life, and more ⚡️🚀🎨',
+  language: 'en-US',
+  theme: 'system',
+  siteUrl: 'https://idkan.dev',
+  siteRepo: 'https://github.com/idkan/idkan.dev',
+  socialMedia:
+    [
+      {
+        name: 'Github',
+        url: 'https://github.com/idkan/'
+      },
+      {
+        name: 'Twitter',
+        url: 'https://twitter.com/idkandev'
+      },
+      {
+        name: 'Linkedin',
+        url: 'https://www.linkedin.com/in/abraham-serena/'
+      },
+      {
+        name: 'Instagram',
+        url: 'https://www.instagram.com/idkan.dev/'
+      }
+    ]
+}
