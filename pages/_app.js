0464dae148ffa7ae714496fd41e4f9e6fe1c14dd
Abraham Serena <abraham.serena.lk@gmail.com>
2022-04-19 12:20:20 -0500

    Updated app layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,22 @@
 import '../styles/globals.scss'
 
-import { ThemeProvider } from 'next-theme'
+import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 import ProgressBar from 'react-scroll-progress-bar'
 
-import { Layout } from '../components'
+import { LayoutWrapper } from '../components'
+import { siteMetadata } from '../data/siteMetadata'
 
 function App ({ Component, pageProps }) {
   return (
-    <ThemeProvider enableSystem attribute='class'>
+    <ThemeProvider enableSystem attribute='class' defaultTheme={siteMetadata.theme}>
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
       <ProgressBar bgcolor='#22c55e' duration='0.2' />
-      <Layout>
+      <LayoutWrapper>
         <Component {...pageProps} />
-      </Layout>
+      </LayoutWrapper>
     </ThemeProvider>
   )
 }
