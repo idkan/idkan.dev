396d2f749eea85e8bd1c6498a89b5823fea03196
Abraham Serena <abraham.serena.lk@gmail.com>
2022-04-19 12:20:54 -0500

    Created Blog layout wrapper

--- components/LayoutWrapper.jsx
+++ components/LayoutWrapper.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { Header } from '.'
 import Footer from './Footer'
 
-const Layout = ({ children }) => {
+const LayoutWrapper = ({ children }) => {
   return (
-    <>
-      <Header />
-      {children}
-      <Footer />
-    </>
+    <div className='mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0'>
+      <div className='flex h-screen flex-col justify-between'>
+        <Header />
+        <main className='mb-auto'>{children}</main>
+        <Footer />
+      </div>
+    </div>
   )
 }
 
-export default Layout
+export default LayoutWrapper
