28bf21830030f639a0c1fbb249f4b0bc17f1c71c
Abraham Serena <abraham.serena.lk@gmail.com>
2022-04-26 18:27:48 -0500

    Adding new navigation links

---
+++ data/headerNavLinks.js
@@ -0,0 +1,6 @@
+export const headerNavLinks = [
+  { href: '/blog', text: 'Blog' },
+  { href: '/tags', text: 'Tags' },
+  { href: '/projects', text: 'Projects' },
+  { href: '/about', text: 'About' }
+]
