9d1fed06d0e8d19105463888112dda44be8b4a7e
Abraham Serena <abraham.serena.lk@gmail.com>
2022-04-25 21:14:20 -0500

    Fixing component manager file

--- components/index.js
+++ components/index.js
@@ -1,9 +1,13 @@
+// Main Sections
 export { default as Header } from './Header'
 export { default as LayoutWrapper } from './LayoutWrapper'
 export { default as Footer } from './Footer'
 
+// Components
 export { default as Tag } from './content/Tag'
-export { default as ProgressBar } from './content/ProgressBar'
+
+// UI
+export { default as ProgressBar } from './ui/ProgressBar'
 
 // Helpers
 export { default as ThemeSwitcher } from './content/ThemeSwitcher'
