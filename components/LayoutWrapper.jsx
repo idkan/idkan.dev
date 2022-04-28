bc3e7e41c38708fcf84961fa236bc7abaa948d45
Abraham Serena <abraham.serena.lk@gmail.com>
2022-04-28 14:50:55 -0500

    Adding css class

--- components/LayoutWrapper.jsx
+++ components/LayoutWrapper.jsx
@@ -5,7 +5,7 @@ const LayoutWrapper = ({ children }) => {
     <div className='mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0'>
       <div className='flex h-screen flex-col justify-between'>
         <Header />
-        <main className='mb-auto'>{children}</main>
+        <main className='relative mb-auto'>{children}</main>
         <Footer />
       </div>
     </div>
