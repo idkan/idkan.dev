2cd5b6a8f2166f8bc019fa7aa214899efb634f28
Abraham Serena <abraham.serena.lk@gmail.com>
2022-04-28 15:39:38 -0500

    Added experience data to author layout

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -1,6 +1,7 @@
 import NextImage from 'next/image'
 import { PageSEO } from '../components/analytics/SEO'
 import SocialIcon from '../components/ui/SocialIcon'
+import { experienceData } from '../data/experienceData'
 
 export default function AuthorLayout ({ children, frontMatter }) {
   const { name, avatar, occupation, company, location, twitter, linkedin, instagram, github } = frontMatter
@@ -35,6 +36,42 @@ export default function AuthorLayout ({ children, frontMatter }) {
             </div>
           </div>
           <div className='prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2'>{children}</div>
+        </div>
+        <div className='mt-10'>
+          <div className='pt-6 pb-8 space-y-2 md:space-y-5'>
+            <h1 className='text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14'>
+              Experience
+            </h1>
+          </div>
+          <div className='py-8 max-w-none xl:col-span-2'>
+            {experienceData.map((experience, index) => (
+              <div key={index} className='my-3'>
+                <div className='flex flex-row text-xl'>
+                  <span className='text-gray-500 dark:text-gray-400'>{experience.title}</span>
+                  <span className='text-gray-500 dark:text-gray-400'>&nbsp;@&nbsp;</span>
+                  <span className='text-primary-500 dark:text-primary-400'>
+                    <a href={experience.companyUrl} className='no-underline'>{experience.company}</a>
+                  </span>
+                </div>
+                <div>
+                  <div className='p-1 font-mono text-sm text-gray-500 dark:text-gray-400'>📅 {experience.rangeDate}</div>
+                  <div className='p-1 font-mono text-sm text-gray-500 dark:text-gray-400'>📍 {experience.location}</div>
+                  <div className='p-2'>
+                    {experience.description.map((description, index) => (
+                      <div key={index} className='flex flex-row space-x-2'>
+                        <div className='text-primary-500 dark:text-primary-400 mr-2 text-lg'>
+                          &#8227;
+                        </div>
+                        <div className='text-gray-500 dark:text-gray-400'>
+                          {description}
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
