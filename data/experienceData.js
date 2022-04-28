7f4c93e10f8756ccefce5e23d86d09d4d55fec41
Abraham Serena <abraham.serena.lk@gmail.com>
2022-04-28 15:38:58 -0500

    Added work experience data

---
+++ data/experienceData.js
@@ -0,0 +1,44 @@
+export const experienceData = [
+  {
+    title: 'Software Engineer',
+    company: 'Kwan Tecnología',
+    companyUrl: 'https://kwantec.com/',
+    location: 'Remote, México',
+    rangeDate: 'Apr 2022 - Present',
+    description: [
+      'Developing and maintaining software for scheduling and managing the operations of multiple companies.',
+      'Working with a variety of technologies, including Java, JavaScript, React, to name a few.'
+    ]
+  },
+  {
+    title: 'Software Engineer',
+    company: 'Softtek',
+    companyUrl: 'https://www.softtek.com/',
+    location: 'Remote, México',
+    rangeDate: 'Jul 2020 - Apr 2022',
+    description: [
+      'Inherent Magento 2 functionalities that are adapted to the needs of the customer.',
+      'Help the client to find the best possible technical solution to solve their problems.',
+      'Designed, built, tested and implemented stand-alone modules.',
+      'Custom theme developed (frontend) based on customer needs and best practices to enhance e-commerce.',
+      'Integrated and custom third-party extensions.',
+      'Track and fix bugs found at some developmental stages.',
+      'Development of the refund functionality with different payment methods as well as the option to run this functionality automatically with cronjobs.'
+    ]
+  },
+  {
+    title: 'Digital Developer Intern',
+    company: 'Softtek',
+    companyUrl: 'https://www.softtek.com/',
+    location: 'Aguascalientes, México',
+    rangeDate: 'Oct 2019 - Jul 2020',
+    description: [
+      'Develop/Training backend solutions using Java, Springboot, JPA.',
+      'Develop front-end solutions using HTML, Javascript, Angular, CSS.',
+      'Create unit tests using JUnit and Mockito.',
+      'Use Test-Driven Design as a software development technique.',
+      'Use Maven to build and test the application and run SQL scripts into the database.'
+    ]
+  }
+
+]
