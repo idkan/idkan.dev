db0510ddce869b649a92924ee6ce99e85dc443fd
Abraham Serena <abraham.serena.lk@gmail.com>
2022-04-28 14:50:28 -0500

    Adding new route to apply tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.js',
+    './lib/**/*.js'
   ],
   darkMode: 'class',
   theme: {
