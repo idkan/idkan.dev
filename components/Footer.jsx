1fef4b7370525f0278ca79ed5f693b05ad4c6b80
Abraham Serena <abraham.serena.lk@gmail.com>
2022-04-30 21:03:47 -0500

    Modified footer layout and information

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 import { siteMetadata } from '../data/siteMetadata'
+import { getCurrentDayName } from '../lib/utils/formatDate'
 
 const Footer = () => {
   return (
@@ -15,11 +16,13 @@ const Footer = () => {
           ))}
         </div>
         <div className='mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400'>
-          <div>{siteMetadata.author}</div>
+          <Link href='/'>
+            <a className='hover:text-primary-600'>{siteMetadata.author}</a>
+          </Link>
           <div>{' • '}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
+          <div>{`Copyright © ${new Date().getFullYear()}`}</div>
           <div>{' • '}</div>
-          <Link href='/'>{siteMetadata.headerTitle}</Link>
+          <div>{`Have a nice ${getCurrentDayName()}!`}</div>
         </div>
         <div className='mb-8 text-sm text-gray-500 dark:text-gray-400'>
           <Image src='/static/images/shiba.gif' alt='idkan.dev' height='24' width='24' className='w-8 h-8 rounded-full mr-4' />
