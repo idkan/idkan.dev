fab4a3da6262d9a418658175a2b5a842740f002d
Abraham Serena <abraham.serena.lk@gmail.com>
2022-05-01 22:33:41 -0500

    Added new information about some projects

---
+++ data/projectsData.js
@@ -0,0 +1,42 @@
+export const projectsData = [
+    {
+        title: 'Personal Website 1.0',
+        description: 'First version of my personal website / Portfolio . It was built using Next.js and TailwindCSS.',
+        imgSrc: '',
+        href: 'https://portfolio-idkan.vercel.app/',
+        github: 'https://github.com/idkan/Portfolio',
+        tech: [ 'Next.js', 'TailwindCSS', 'React', 'Sass']  
+    },
+    {
+        title: 'Crypto World',
+        description: 'A web application that shows the top 100 cryptocurrencies by market cap.',
+        imgSrc: '',
+        href: 'https://crypto-world-pi.vercel.app/',
+        github: 'https://github.com/idkan/Crypto-World',
+        tech: [ 'React', 'Redux', 'Javascript']
+    },
+    {
+        title: 'RentMe',
+        description: 'A web application that allows users to rent or buy a House in a fast and easy way.',
+        imgSrc: '',
+        href: 'https://rent-me.vercel.app/',
+        github: 'https://github.com/idkan/RentMe',
+        tech: [ 'Next.js', 'Javascript']
+    },
+    {
+        title: 'Travely',
+        description: 'Travel advisor web application that allows users to search for the best places to visit or eat in a specific area.',
+        imgSrc: '',
+        href: 'https://travely-kohl.vercel.app/',
+        github: 'https://github.com/idkan/Travely',
+        tech: [ 'React', 'Javascript']
+    },
+    {
+        title: 'Budget Tracker',
+        description: 'A web application that allows users to track their expenses and incomes.',
+        imgSrc: '',
+        href: '',
+        github: 'https://github.com/idkan/budget-tracker',
+        tech: [ 'Next.js', 'Javascript', 'TailwindCSS']
+    }
+] 
