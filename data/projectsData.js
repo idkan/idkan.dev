c655e97729f81da15263722ad54bef41885c8ca9
Abraham Serena <abraham.serena.lk@gmail.com>
2022-05-03 17:38:41 -0500

    Added new personal project information

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,5 +1,13 @@
 export const projectsData = [
   {
+    title: 'Budget Tracker',
+    description: 'A web application that allows users to track their expenses and incomes.',
+    imgSrc: '',
+    href: '',
+    github: 'https://github.com/idkan/budget-tracker',
+    tech: ['Next.js', 'Javascript', 'TailwindCSS']
+  },
+  {
     title: 'Personal Website 1.0',
     description: 'First version of my personal website / Portfolio . It was built using Next.js and TailwindCSS.',
     imgSrc: '',
@@ -32,11 +40,11 @@ export const projectsData = [
     tech: ['React', 'Javascript']
   },
   {
-    title: 'Budget Tracker',
-    description: 'A web application that allows users to track their expenses and incomes.',
+    title: 'Car Classifier',
+    description: 'A Python Machine Learning using CNN that allows users to recognize cars based on their models.',
     imgSrc: '',
     href: '',
-    github: 'https://github.com/idkan/budget-tracker',
-    tech: ['Next.js', 'Javascript', 'TailwindCSS']
+    github: 'https://github.com/idkan/Car-Classifier',
+    tech: ['Python', 'Keras', 'Tensorflow']
   }
 ]
