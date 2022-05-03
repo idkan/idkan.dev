d42f1da4bc31d9652cc4062a7242ac051252df1e
Abraham Serena <abraham.serena.lk@gmail.com>
2022-05-03 17:38:54 -0500

    Adding List of post layout

---
+++ layouts/ListLayout.js
@@ -0,0 +1,96 @@
+import Link from 'next/link'
+import { useState } from 'react'
+import { Tag } from '../components'
+import { formatDate } from '../lib/utils/formatDate'
+
+export default function ListLayout ({ posts, title, initialDisplayPosts = [], pagination }) {
+  const [searchValue, setSearchValue] = useState('')
+  const filteredPosts = posts.filter((frontMatter) => {
+    const searchContent = frontMatter.title + frontMatter.summary + frontMatter.tags.join(' ')
+    return searchContent.toLowerCase().includes(searchValue.toLowerCase())
+  })
+
+  const displayPosts = initialDisplayPosts.length > 0 && !searchValue ? initialDisplayPosts : filteredPosts
+
+  return (
+    <>
+      <div className='divide-y'>
+        <div className='space-y-2 pt-6 pb-8 md:space-y-5'>
+          <h1 className='text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14'>
+            {title}
+          </h1>
+          <div className='relative mx-w-lg'>
+            <input
+              aria-label='Search Post'
+              type='text'
+              onChange={(e) => setSearchValue(e.target.value)}
+              placeholder='Search posts'
+              className='block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900  focus:outline-none focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100 dark:focus:outline-none dark:focus:border-primary-600 dark:focus:ring-primary-600'
+            />
+            <svg
+              className='absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z' />
+            </svg>
+          </div>
+        </div>
+        <ul>
+          {!filteredPosts.length && 'No posts found'}
+          {displayPosts.map((frontMatter) => {
+            const { slug, title, summary, tags, date } = frontMatter
+            return (
+              <li key={slug} className='py-4'>
+                <article className='space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0'>
+                  <dl>
+                    <dt className='sr-only'>Published on</dt>
+                    <dd className='text-base font-medium leading-6 text-gray-500 dark:text-gray-400'>
+                      <time dateTime={date}>{formatDate(date)}</time>
+                    </dd>
+                  </dl>
+                  <div className='space-y-3 xl:col-span-3'>
+                    <div>
+                      <h3 className='text-2xl font-bold leading-8 tracking-tight'>
+                        <Link href={`/blog/${slug}`}>
+                          <a className='text-gray-900 dark:text-gray-100'>{title}</a>
+                        </Link>
+                      </h3>
+                      <div className='flex flex-wrap'>
+                        {tags.map((tag) => (
+                          <Tag key={tag} tag={tag} />
+                        ))}
+                      </div>
+                    </div>
+                    <div className='prose max-w-none text-gray-500 dark:text-gray-400'>
+                      {summary}
+                    </div>
+                  </div>
+                </article>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      {pagination && pagination.totalPages > 1 && !searchValue && (
+        <div className='flex justify-center py-6'>
+          <nav className='space-x-4'>
+            <Link href={`/blog?page=${pagination.currentPage - 1}`}>
+              <a className='text-base font-medium leading-6 text-gray-500 hover:text-gray-900 dark:text-gray-400 focus:outline-none focus:text-gray-900 focus:bg-gray-50 focus:border-gray-300 dark:focus:text-gray-100 dark:focus:bg-gray-50 dark:focus:border-gray-300'>
+                Previous
+              </a>
+            </Link>
+            <Link href={`/blog?page=${pagination.currentPage + 1}`}>
+              <a className='text-base font-medium leading-6 text-gray-500 hover:text-gray-900 dark:text-gray-400 focus:outline-none focus:text-gray-900 focus:bg-gray-50 focus:border-gray-300 dark:focus:text-gray-100 dark:focus:bg-gray-50 dark:focus:border-gray-300'>
+                Next
+              </a>
+            </Link>
+          </nav>
+        </div>
+
+      )}
+    </>
+
+  )
+}
