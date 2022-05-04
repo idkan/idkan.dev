fa3f200f91d79be78f7d533bd4d34e47ebd6fda4
Abraham Serena <abraham.serena.lk@gmail.com>
2022-05-04 16:13:23 -0500

    Adding pagination to components manager file

--- components/index.js
+++ components/index.js
@@ -6,6 +6,7 @@ export { default as Footer } from './Footer'
 // Components
 export { default as Tag } from './content/Tag'
 export { default as Card } from './content/Card'
+export { default as Pagination } from './content/Pagination'
 
 // UI
 export { default as ProgressBar } from './ui/ProgressBar'
