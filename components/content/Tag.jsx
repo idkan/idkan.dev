4bcf49c26e3e71a4b1b9a20c996e18d65ff91bce
Abraham Serena <abraham.serena.lk@gmail.com>
2022-05-05 23:43:28 -0500

    Fixed tag route

--- components/content/Tag.jsx
+++ components/content/Tag.jsx
@@ -3,7 +3,7 @@ import { kebabCase } from '../../lib/utils/kebabCaseFormat'
 
 const Tag = ({ tag }) => {
   return (
-    <Link href={`/tag/${kebabCase(tag)}`}>
+    <Link href={`/tags/${kebabCase(tag)}`}>
       <a className='mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'>
         {tag}
       </a>
