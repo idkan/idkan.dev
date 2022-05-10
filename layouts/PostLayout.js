17873a7a77adf34c7e8653491b1500cd829c729b
Abraham Serena <abraham.serena.lk@gmail.com>
2022-05-10 18:13:50 -0500

    Addede new data into post layout

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -1,11 +1,20 @@
 import NextImage from 'next/image'
+import Link from 'next/link'
 import { siteMetadata } from '../data/siteMetadata'
+import { Tag } from '../components/index'
+import { BlogSEO } from '../components/analytics/SEO'
 
 export default function PostLayout ({ frontMatter, authorDetails, prevPost, nextPost, children }) {
   const { slug, fileName, title, date, tags } = frontMatter
 
   return (
     <div className='max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0'>
+      <BlogSEO
+        url={`${siteMetadata.siteUrl}/blog/${slug}`}
+        authorDetails={authorDetails}
+        {...frontMatter}
+      />
+      {/* <ScrollTopAndComment /> */}
       <article>
         <div className='xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700'>
           <header className='pt-6 xl:pb-6'>
@@ -13,7 +22,7 @@ export default function PostLayout ({ frontMatter, authorDetails, prevPost, next
               <dl className='space-y-10'>
                 <div>
                   <dt className='sr-only'>Published on</dt>
-                  <dd className='text-base font-medium leading-6 text-slate-500 dark:text-slate-400'>
+                  <dd className='text-base font-medium leading-6 text-gray-500 dark:text-gray-400'>
                     <time dateTime={date}>
                       {new Date(date).toLocaleDateString(siteMetadata.locale, {
                         weekday: 'long',
@@ -26,39 +35,39 @@ export default function PostLayout ({ frontMatter, authorDetails, prevPost, next
                 </div>
               </dl>
               <div>
-                <h1 className='text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-10'>
-                  {title}
-                </h1>
+                <h1 className='text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-10'>{title}</h1>
               </div>
             </div>
           </header>
-          <div className='pb-8 divide-y divide-gray-200 xl:divide-y-0 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6' style={{ gridTemplateRows: 'auto 1fr' }}>
-            <dl className='pt-6 pb-10 xl:pt-11 xl:border-b xl:border-gray-200 xl:dark:border-gray-700'>
+          <div
+            className='divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0'
+            style={{ gridTemplateRows: 'auto 1fr' }}
+          >
+            <dl className='pt-6 pb-10 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700'>
               <dt className='sr-only'>Authors</dt>
               <dd>
-                <ul className='flex justify-center space-x-8 xl:block sm:space-x-12 xl:space-x-0 xl:space-y-8'>
+                <ul className='flex justify-center space-x-8 sm:space-x-12 xl:block xl:space-x-0 xl:space-y-8'>
                   {authorDetails.map((author) => (
-                    <li key={author.name} className='flex items-center space-x-2'>
+                    <li className='flex items-center space-x-2' key={author.name}>
                       {author.avatar && (
                         <NextImage
                           src={author.avatar}
-                          width={40}
-                          height={40}
-                          className='rounded-full'
-                          alt={author.name}
+                          width='38px'
+                          height='38px'
+                          alt='avatar'
+                          className='h-10 w-10 rounded-full'
                         />
                       )}
-                      <dl className='text-sm font-medium leading-5 whitespace-nowrap'>
+                      <dl className='whitespace-nowrap text-sm font-medium leading-5'>
                         <dt className='sr-only'>Name</dt>
-                        <dd className='text-slate-900 dark:text-slate-100'>{author.name}</dd>
+                        <dd className='text-gray-900 dark:text-gray-100'>{author.name}</dd>
                         <dt className='sr-only'>Twitter</dt>
                         <dd>
-                          <a
-                            href={`https://twitter.com/${author.twitter}`}
-                            className='text-primary-500 dark:text-primary-600 hover:text-primary-400 dark:hover:text-primary-500'
-                          >
-                            {author.twitter.replace('https://twitter.com/', '@')}
-                          </a>
+                          {author.twitter && (
+                            <a href={author.twitter} target='_blank' rel='noopener noreferrer' className='text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'>
+                              {author.twitter.replace('https://twitter.com/', '@')}
+                            </a>
+                          )}
                         </dd>
                       </dl>
                     </li>
@@ -66,9 +75,71 @@ export default function PostLayout ({ frontMatter, authorDetails, prevPost, next
                 </ul>
               </dd>
             </dl>
+            <div className='divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0'>
+              <div className='prose max-w-none pt-10 pb-8 dark:prose-dark'>{children}</div>
+              <div className='pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300'>
+                {/* <Link href={discussUrl(slug)} rel='nofollow'>
+                  Discuss on Twitter
+                </Link> */}
+                {' • '}
+                {/* <Link href={editUrl(fileName)}>View on GitHub</Link> */}
+              </div>
+              {/* <Comments frontMatter={frontMatter} /> */}
+            </div>
+            <footer>
+              <div className='divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y'>
+                {tags && (
+                  <div className='py-4 xl:py-8'>
+                    <h2 className='text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400'>
+                      Tags
+                    </h2>
+                    <div className='flex flex-wrap'>
+                      {tags.map((tag) => (
+                        <Tag key={tag} tag={tag} />
+                      ))}
+                    </div>
+                  </div>
+                )}
+                {(nextPost || prevPost) && (
+                  <div className='flex justify-between py-4 xl:block xl:space-y-8 xl:py-8'>
+                    {prevPost && (
+                      <div>
+                        <h2 className='text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400'>
+                          Previous Article
+                        </h2>
+                        <div className='text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'>
+                          <Link href={`/blog/${prevPost.slug}`}>{prevPost.title}</Link>
+                        </div>
+                      </div>
+                    )}
+                    {nextPost && (
+                      <div>
+                        <h2 className='text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400'>
+                          Next Article
+                        </h2>
+                        <div className='text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'>
+                          <Link href={`/blog/${nextPost.slug}`}>
+                            <a href={`/blog/${nextPost.slug}`}>{nextPost.title}</a>
+                          </Link>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                )}
+              </div>
+              <div className='pt-4 xl:pt-8'>
+                <Link
+                  href='/blog'
+                  className='text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'
+                >
+                  <a href='/blog'>&larr; Back to the blog</a>
+                </Link>
+              </div>
+            </footer>
           </div>
         </div>
       </article>
     </div>
+
   )
 }
