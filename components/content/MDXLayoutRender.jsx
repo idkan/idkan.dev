e388ac519bf5a29d4aec58aa64591d753cbb666f
Abraham Serena <abraham.serena.lk@gmail.com>
2022-05-11 16:03:22 -0500

    Addede new componentes to MDX Layout

--- components/content/MDXLayoutRender.jsx
+++ components/content/MDXLayoutRender.jsx
@@ -1,7 +1,13 @@
 import { useMemo } from 'react'
 import { getMDXComponent } from 'mdx-bundler/client'
 
+import { Image, TOCInline, CustomLink, Pre } from '../index'
+
 export const MDXComponents = {
+  Image,
+  TOCInline,
+  a: CustomLink,
+  pre: Pre,
   wrapper: ({ components, layout, ...rest }) => {
     const Layout = require(`../../layouts/${layout}`).default
     return <Layout {...rest} />
