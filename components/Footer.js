034877fb864042dca2df39096e07ba27a9bc24d3
Abraham Serena <abraham.serena.lk@gmail.com>
2022-05-15 15:27:37 -0500

    Renaming Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,6 @@
-import Link from 'next/link'
-import Image from 'next/image'
-
 import { siteMetadata } from '../data/siteMetadata'
 import { getCurrentDayName } from '../lib/utils/formatDate'
+import { Link, Image } from '../components'
 
 const Footer = () => {
   return (
@@ -10,15 +8,13 @@ const Footer = () => {
       <div className='mt-16 flex flex-col items-center'>
         <div className='mb-3 flex space-x-4'>
           {siteMetadata.socialMedia.map((item, key) => (
-            <a key={key} href={item.url} target='_blank' rel='noopener noreferrer' className='text-gray-500 hover:text-primary-600'>
+            <Link key={key} href={item.url} className='text-gray-500 hover:text-primary-600'>
               {item.icon}
-            </a>
+            </Link>
           ))}
         </div>
         <div className='mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400'>
-          <Link href='/'>
-            <a className='hover:text-primary-600'>{siteMetadata.author}</a>
-          </Link>
+          <Link href='/' className='hover:text-primary-600'>{siteMetadata.author}</Link>
           <div>{' • '}</div>
           <div>{`Copyright © ${new Date().getFullYear()}`}</div>
           <div>{' • '}</div>
