a9bdb2649d6bf149afbd17765d3f990ffe1400f2
Abraham Serena <abraham.serena.lk@gmail.com>
2022-05-17 16:43:30 -0500

    Adding helper component to render post title and date

---
+++ components/content/PostHeaderWrapper.js
@@ -0,0 +1,21 @@
+import { formatDate } from '../../lib/utils/formatDate'
+
+export default function Header ({ date, title }) {
+  return (
+    <div className='header'>
+      <div className='space-y-1 border-b border-gray-200 pb-10 text-center dark:border-gray-700'>
+        <dl>
+          <div>
+            <dt className='sr-only'>Published on</dt>
+            <dd className='text-base font-medium leading-6 text-gray-500 dark:text-gray-400'>
+              <time dateTime={date}>{formatDate(date)}</time>
+            </dd>
+          </div>
+        </dl>
+        <div>
+          <h1 className='text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-10'>{title}</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
