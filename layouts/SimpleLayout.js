f4ec744f7192b7f2b7f9db5617a0dae513dc4cda
Abraham Serena <abraham.serena.lk@gmail.com>
2022-05-16 20:09:30 -0500

    Added tags and pagination to simple post layout

--- layouts/SimpleLayout.js
+++ layouts/SimpleLayout.js
@@ -1,13 +1,13 @@
-import { Link } from '../components'
+import { Tag, Link } from '../components'
 import { BlogSEO } from '../components/analytics/SEO'
 import { siteMetadata } from '../data/siteMetadata'
 import { formatDate } from '../lib/utils/formatDate'
 
 export default function SimpleLayout ({ frontMatter, authorDetails, prevPost, nextPost, children }) {
-  const { date, title } = frontMatter
+  const { slug, title, date, tags } = frontMatter
   return (
     <div className='max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0'>
-      <BlogSEO url={`${siteMetadata.siteUrl}/blog/${frontMatter.slug}`} {...frontMatter} />
+      <BlogSEO url={`${siteMetadata.siteUrl}/blog/${slug}`} {...frontMatter} />
       {/* <ScrollTopAndComment /> */}
       <article>
         <div>
@@ -32,21 +32,46 @@ export default function SimpleLayout ({ frontMatter, authorDetails, prevPost, ne
             </div>
             {/* <Comments frontMatter={frontMatter} /> */}
             <div className='footer'>
-              <div className='flex flex-col text-sm font-medium sm:flex-row sm:justify-between sm:text-base'>
-                {prevPost && (
-                  <div className='pt-4 xl:pt-8'>
-                    <Link href={`/blog/${prevPost.slug}`} className='text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'>
-                      &larr; {prevPost.title}
-                    </Link>
+              <div className='divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y'>
+                {tags && (
+                  <div className='py-4 xl:py-8'>
+                    <h2 className='text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400'>
+                      Tags
+                    </h2>
+                    <div className='flex flex-wrap'>
+                      {tags.map((tag) => (
+                        <Tag key={tag} tag={tag} />
+                      ))}
+                    </div>
                   </div>
                 )}
-                {nextPost && (
-                  <div className='pt-4 xl:pt-8'>
-                    <Link href={`/blog/${nextPost.slug}`} className='text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'>
-                      {nextPost.title} &rarr;
-                    </Link>
+                {(nextPost || prevPost) && (
+                  <div className='flex justify-between py-4 xl:block xl:space-y-8 xl:py-8'>
+                    {prevPost && (
+                      <div>
+                        <h2 className='text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400'>
+                          Previous Article
+                        </h2>
+                        <div className='text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'>
+                          <Link href={`/blog/${prevPost.slug}`}>{prevPost.title}</Link>
+                        </div>
+                      </div>
+                    )}
+                    {nextPost && (
+                      <div>
+                        <h2 className='text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400'>
+                          Next Article
+                        </h2>
+                        <div className='text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'>
+                          <Link href={`/blog/${nextPost.slug}`}>{nextPost.title}</Link>
+                        </div>
+                      </div>
+                    )}
                   </div>
                 )}
+              </div>
+              <div className='pt-4 xl:pt-8'>
+                <Link href='/blog' className='text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'>&larr; Back to the blog</Link>
               </div>
             </div>
           </div>
