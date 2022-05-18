674d94a31f6b224213b749d6d8e580aab48cb0e5
Abraham Serena <abraham.serena.lk@gmail.com>
2022-05-18 16:50:13 -0500

    Adding information about comment service

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -33,5 +33,24 @@ export const siteMetadata = {
       url: 'https://www.instagram.com/idkan.dev/',
       icon: <BsInstagram size='24' />
     }
-  ]
+  ],
+  comment: {
+    provider: 'giscus',
+    giscusConfig: {
+      // Visit the link below, and follow the steps in the 'configuration' section
+      // https://giscus.app/
+      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
+      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      mapping: 'pathname',
+      reactions: '1',
+      metadata: '0',
+      theme: 'light',
+      inputPosition: 'bottom',
+      lang: 'en',
+      darkTheme: 'dark_dimmed',
+      themeURL: ''
+    }
+  }
 }
