72ae6060770bea04e3d60d5b5f8bd02fb6ea2ecb
Abraham Serena <abraham.serena.lk@gmail.com>
2022-05-18 17:33:04 -0500

    Refactoring simple layout

--- layouts/SimpleLayout.js
+++ layouts/SimpleLayout.js
@@ -2,7 +2,7 @@ import { ScrollTopAndComment, PostHeader, PostContent } from '../components'
 import { BlogSEO } from '../components/analytics/SEO'
 import { siteMetadata } from '../data/siteMetadata'
 
-export default function SimpleLayout ({ frontMatter, authorDetails, prevPost, nextPost, children }) {
+export default function SimpleLayout ({ frontMatter, prevPost, nextPost, children }) {
   return (
     <div className='max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0'>
       <BlogSEO url={`${siteMetadata.siteUrl}/blog/${frontMatter.slug}`} {...frontMatter} />
@@ -11,7 +11,7 @@ export default function SimpleLayout ({ frontMatter, authorDetails, prevPost, ne
         <div>
           <PostHeader title={frontMatter.title} date={frontMatter.date} />
           <div className='divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:divide-y-0' style={{ gridTemplateRows: 'auto 1fr' }}>
-            <PostContent postData={children} tags={frontMatter.tags} prevPost={prevPost} nextPost={nextPost} />
+            <PostContent postData={children} prevPost={prevPost} nextPost={nextPost} props={frontMatter} />
           </div>
         </div>
       </article>
